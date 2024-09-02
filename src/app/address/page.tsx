a1251c1fb99ac4316e5860defffcbf7440dee9a8
HAFSA <hafsa.strategist@gmail.com>
2024-09-02 09:58:05 -0700

    template

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -7,24 +7,25 @@ const Page = () => {
       <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
         <Image
           className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src="/Container Home.jpeg"
-          alt="my home"
+          src="/Location icon.jpeg"
+          alt="Sarah Dayan"
           width="384"
           height="512"
         />
         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p className="text-lg font-medium">
-              Home is where the heart finds its true rythym,a heaven where the soul can unwind.We find comfort and belonging,
-              a sanctuary where the everyday magic of life unholds.It is more than just walls and roofs,it is the heart of our everyday lives.
+            House No. 12, Street 5
+        Gulshan-e-Iqbal, Karachi
+        Pakistan
             </p>
           </blockquote>
           <figcaption className="font-medium">
             <div className="text-sky-500 dark:text-sky-400">
-              hafsa azhar
+              Hafsa azhar
             </div>
             <div className="text-slate-700 dark:text-slate-500">
-              student, ryk
+              Student, ryk
             </div>
           </figcaption>
         </div>
